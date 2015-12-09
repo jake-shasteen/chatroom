4eee3e1807648536d929809b112e6ea7c694b7d3
Jake Shasteen <jakeshasteen@gmail.com>
2015-12-09 17:47:49 -0500

    Add connect / disconnect message

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ app.get( '/', function( req, res ) {
 
 io.on( 'connection', function( socket ) {
 	console.log( 'a user connected.' );
+	io.emit( 'chat message', "A user has connected." );
 	socket.on( 'disconnect', function() {
 		console.log( 'a user disconnected.' );
+		io.emit( 'chat message', "A user has disconnected." );
 	});
 	socket.on( 'chat message', function( msg ) {
 		console.log( 'message: ' + msg );
